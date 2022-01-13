aed51925ff4f30363054bfb7ce40dbad436752b1
unknown <salilbagga2@gmail.com>
2022-01-13 23:45:45 +0530

    React Weather App 1.4

--- src/hooks/useFetchdata.js
+++ src/hooks/useFetchdata.js
@@ -7,27 +7,26 @@ export const useFetchdata = (input) => {
   const [invalidInput, setInvalidInput] = useState(false);
   const [data, setData] = useState(null);
 
-  const fetchdata = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=${API_KEY}`;
-
-    setError(null);
-    // sign the user out
-    try {
-      const res = await fetch(url);
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      const data = await res.json();
-      setData(data);
-      setError(null);
-      setInvalidInput(false);
-    } catch (err) {
-      setError(err.message);
-      setData(null);
-    }
-  };
-
   useEffect(() => {
+    const fetchdata = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=${API_KEY}`;
+
+      setError(null);
+      // sign the user out
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const data = await res.json();
+        setData(data);
+        setError(null);
+        setInvalidInput(false);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      }
+    };
     if (input) {
       fetchdata();
     }
--- src/hooks/useFetchlangdata.js
+++ src/hooks/useFetchlangdata.js
@@ -8,27 +8,26 @@ export const useFetchlangdata = (input) => {
   const [invalidInput, setInvalidInput] = useState(false);
   const [data, setData] = useState(null);
 
-  const fetchlangdata = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${input[0]}&lon=${input[1]}&appid=${API_KEY}`;
-
-    setError(null);
-    // sign the user out
-    try {
-      const res = await fetch(url);
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      const data = await res.json();
-      setData(data);
-      setError(null);
-      setInvalidInput(false);
-    } catch (err) {
-      setError(err.message);
-      setData(null);
-    }
-  };
-
   useEffect(() => {
+    const fetchlangdata = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${input[0]}&lon=${input[1]}&appid=${API_KEY}`;
+
+      setError(null);
+      // sign the user out
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const data = await res.json();
+        setData(data);
+        setError(null);
+        setInvalidInput(false);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      }
+    };
     if (input) {
       fetchlangdata();
     }
